e6afce3354909927da26f422e1ac4dae6afc7afb
Harrison White <harrisonkwhite@gmail.com>
2025-06-18 19:22:31 +1000

    Add player rendering.

--- src/game.c
+++ src/game.c
@@ -1,12 +1,31 @@
 #include <stdlib.h>
 #include <zfw_game.h>
 
+typedef enum {
+    ek_sprite_player,
+
+    eks_sprite_cnt
+} e_sprite;
+
+s_rect_i g_sprite_src_rects[eks_sprite_cnt] = {
+    {0, 0, 24, 24}
+};
+
 typedef struct {
-    int a;
+    s_textures textures;
 } s_game;
+
+static const char* TextureIndexToFilePath(const int index) {
+    return "../assets/sprites.png";
+}
 
 static bool InitGame(const s_game_init_func_data* const func_data) {
     s_game* const game = func_data->user_mem;
+
+    if (!LoadTexturesFromFiles(&game->textures, func_data->perm_mem_arena, 1, TextureIndexToFilePath)) {
+            return false;
+    }
+
     return true;
 }
 
@@ -15,8 +34,28 @@ static bool GameTick(const s_game_tick_func_data* const func_data) {
     return true;
 }
 
+static inline void RenderSprite(const s_rendering_context* const context, const int sprite_index, const s_textures* const textures, const s_vec_2d pos, const s_vec_2d origin, const s_vec_2d scale, const float rot, const s_color blend) {
+    RenderTexture(
+        context,
+        0,
+        textures,
+        g_sprite_src_rects[sprite_index],
+        pos,
+        origin,
+        scale,
+        rot,
+        blend
+    );
+}
+
 static bool RenderGame(const s_game_render_func_data* const func_data) {
     s_game* const game = func_data->user_mem;
+
+    RenderClear((s_color){0.2, 0.3, 0.4, 1.0});
+
+    RenderSprite(&func_data->rendering_context, ek_sprite_player, &game->textures, VEC_2D_ZERO, (s_vec_2d){0.5f, 0.5f}, (s_vec_2d){4.0f, 4.0f}, 0.0f, WHITE);
+    Flush(&func_data->rendering_context);
+
     return true;
 }
 
