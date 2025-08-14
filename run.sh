d2ddab97d153a606a05f310d4752cd135a0e0c5f
Harrison White <harrisonkwhite@gmail.com>
2025-08-15 00:36:15 +1000

    Add target for packing assets.

--- run.sh
+++ run.sh
@@ -6,9 +6,4 @@ cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cmake --build .
 cp compile_commands.json ..
 cd ..
-mkdir -p assets
-mkdir -p assets/textures
-mkdir -p assets/fonts
-mkdir -p assets/shader_progs
-build/zeta_framework/zfw_asset_packer/zfw_asset_packer
 build/terraria_clone
