4ef2cba465ad46992bda16a9a0ee29e4adc41774
Harrison White <harrisonkwhite@gmail.com>
2025-12-19 09:03:09 +1000

    Have scripts not run executable if compilation failed.

--- scripts/windows_ninja/run_debug.ps1
+++ scripts/windows_ninja/run_debug.ps1
@@ -1,12 +1,15 @@
 $ErrorActionPreference = "Stop"
+
 $ProjectRoot = (Resolve-Path "$PSScriptRoot\..\..").Path
 $BuildDir = Join-Path $ProjectRoot "build/debug"
 
 cmake -S "$ProjectRoot" -B "$BuildDir" -G Ninja -DCMAKE_BUILD_TYPE=Debug
+if ($LASTEXITCODE -ne 0) { exit 1 }
+
 cmake --build "$BuildDir"
+if ($LASTEXITCODE -ne 0) { exit 1 }
 
 Push-Location $BuildDir
-
 try {
     & ".\kaozeth.exe"
 }
--- scripts/windows_ninja/run_release.ps1
+++ scripts/windows_ninja/run_release.ps1
@@ -1,12 +1,15 @@
 $ErrorActionPreference = "Stop"
+
 $ProjectRoot = (Resolve-Path "$PSScriptRoot\..\..").Path
 $BuildDir = Join-Path $ProjectRoot "build/release"
 
 cmake -S "$ProjectRoot" -B "$BuildDir" -G Ninja -DCMAKE_BUILD_TYPE=Release
+if ($LASTEXITCODE -ne 0) { exit 1 }
+
 cmake --build "$BuildDir"
+if ($LASTEXITCODE -ne 0) { exit 1 }
 
 Push-Location $BuildDir
-
 try {
     & ".\kaozeth.exe"
 }
