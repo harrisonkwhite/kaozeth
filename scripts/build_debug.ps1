80cb9a2bc61e3d8fb82e82d4308106878172e640
Harrison White <harrisonkwhite@gmail.com>
2025-12-18 18:24:26 +1000

    Something!

--- scripts/build_debug.ps1
+++ scripts/build_debug.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = "Stop"
 $ProjectRoot = Resolve-Path "$PSScriptRoot\.."
-cmake -S $ProjectRoot -B "$ProjectRoot/build/debug" -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -S $ProjectRoot -B "$ProjectRoot/build/debug" -G Ninja -DCMAKE_BUILD_TYPE=Debug
 cmake --build "$ProjectRoot/build/debug"
--- scripts/build_release.ps1
+++ scripts/build_release.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = "Stop"
 $ProjectRoot = Resolve-Path "$PSScriptRoot\.."
-cmake -S $ProjectRoot -B "$ProjectRoot/build/release" -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -S $ProjectRoot -B "$ProjectRoot/build/release" -G Ninja -DCMAKE_BUILD_TYPE=Release
 cmake --build "$ProjectRoot/build/release"
