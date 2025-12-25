98e6389964f3b213a2852ccf4e2e83fcc5ffa005
Harrison White <harrisonkwhite@gmail.com>
2025-12-25 17:26:40 +1000

    Yeah!

--- src/game.cpp
+++ src/game.cpp
@@ -9,15 +9,19 @@ void GameInit(const zf::s_game_init_context &zf_context) {
     zf::SetWindowTitle(g_game_title, zf_context.temp_mem_arena);
     zf::SetCursorVisibility(false);
 
-    if (!zf::CreateTextureResourceFromPacked(zf::s_cstr_literal("assets/textures/player.zfd"), zf_context.temp_mem_arena, g_game.player_texture)) {
+    if (!zf::PermGFXResourceArena().AddTextureFromPacked(zf::s_cstr_literal("assets/textures/player.zfd"), zf_context.temp_mem_arena, g_game.player_texture)) {
         ZF_FATAL();
     }
 
-    if (!zf::CreateTextureResourceFromPacked(zf::s_cstr_literal("assets/textures/enemy.zfd"), zf_context.temp_mem_arena, g_game.enemy_texture)) {
+    if (!zf::PermGFXResourceArena().AddTextureFromPacked(zf::s_cstr_literal("assets/textures/enemy.zfd"), zf_context.temp_mem_arena, g_game.enemy_texture)) {
         ZF_FATAL();
     }
 
     if (!zf::CreateFontFromPacked(zf::s_cstr_literal("assets/fonts/arial.zfd"), zf_context.temp_mem_arena, g_game.font)) {
+        ZF_FATAL();
+    }
+
+    if (!g_game.snd_type_arena.AddFromPacked(zf::s_cstr_literal("assets/sounds/explosion.zfd"), zf_context.temp_mem_arena, g_game.snd_types.explosion)) {
         ZF_FATAL();
     }
 }
@@ -37,6 +41,16 @@ void GameTick(const zf::s_game_tick_context &zf_context) {
     g_game.player.pos += g_game.player.vel;
 
     g_game.player.rot = zf::CalcDirInRads(g_game.player.pos, zf_context.input_state.cursor_pos);
+
+    if (zf::IsKeyPressed(zf_context.input_state, zf::ek_key_code_space)) {
+        zf::PlaySound(*g_game.snd_types.explosion);
+    }
+
+    for (zf::t_i32 i = 0; i < zf::g_gamepad_limit; i++) {
+        if (zf::IsGamepadConnected(zf_context.input_state, i)) {
+            zf::Log(zf::s_cstr_literal("Axis: %"), zf::GamepadAxisValueRaw(zf_context.input_state, i, zf::ek_gamepad_axis_code_left_x));
+        }
+    }
 }
 
 void GameRender(const zf::s_game_render_context &zf_context) {
@@ -46,4 +60,6 @@ void GameRender(const zf::s_game_render_context &zf_context) {
     zf::DrawStr(zf_context.rendering_context, zf::s_cstr_literal("Dude, man"), g_game.font, {}, zf_context.temp_mem_arena);
 }
 
-void GameCleanup() {}
+void GameCleanup() {
+    g_game.snd_type_arena.Release();
+}
--- src/game.h
+++ src/game.h
@@ -7,18 +7,24 @@ constexpr zf::s_cstr_literal g_game_title = "Kaōzeth";
 constexpr zf::s_v2 g_cursor_size = {4.0f, 4.0f};
 
 struct s_player {
-    zf::s_v2 pos;
-    zf::s_v2 vel;
+    zf::s_v2 pos = {};
+    zf::s_v2 vel = {};
     zf::t_f32 rot = 0.0f;
 };
 
 struct s_game {
-    zf::s_ptr<zf::s_gfx_resource> player_texture;
-    zf::s_ptr<zf::s_gfx_resource> enemy_texture;
+    zf::s_ptr<zf::s_gfx_resource> player_texture = nullptr;
+    zf::s_ptr<zf::s_gfx_resource> enemy_texture = nullptr;
 
-    zf::s_font font;
+    zf::s_font font = {};
 
-    s_player player;
+    zf::s_sound_type_arena snd_type_arena;
+
+    struct {
+        zf::s_ptr<zf::s_sound_type> explosion;
+    } snd_types;
+
+    s_player player = {};
 };
 
 void GameInit(const zf::s_game_init_context &zf_context);
